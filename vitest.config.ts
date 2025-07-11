2629faf54fa5c8046fb7f70bfc2e0b178252e91b
taiyme <53635909+taiyme@users.noreply.github.com>
2025-07-11 18:12:34 +0900

    chore(ci): fix vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import { vCache } from '@raegen/vite-plugin-vitest-cache';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     vCache({
       dir: 'node_modules/.cache/vitest',
     }),
